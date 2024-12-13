4a31e853125f2026fe22973a7c2f5900c628ecd0
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-12-13 11:48:08 +0100

    dasasd

--- flake.nix
+++ flake.nix
@@ -13,7 +13,50 @@
     {
       packages = forAllSystems (system: rec {
         default = pkgs.${system}.callPackage ./default.nix { };
-        nixosModule = pkgs.${system}.callPackage ./module.nix { inherit default; };
+        # nixosModule = pkgs.${system}.callPackage ./module.nix { inherit default; };
+        nixosModule =
+          {
+            config,
+            lib,
+            default,
+            ...
+          }:
+          let
+            cfg = config.services.ravensiris-web;
+            user = "ravensiris-web";
+            dataDir = "/var/lib/ravensiris-web";
+          in
+          {
+            options.services.ravensiris-web = {
+              enable = lib.mkEnableOption "ravensiris-web";
+            };
+            config = lib.mkIf cfg.enable {
+              environment.systemPackages = [ default ];
+
+              users.users.${user} = {
+                isSystemUser = true;
+                group = user;
+                home = dataDir;
+                createHome = true;
+              };
+              users.groups.${user} = { };
+
+              systemd.services = {
+                ravensiris-web = {
+                  description = "Start up the homepage";
+                  wantedBy = [ "multi-user.target" ];
+                  script = ''
+                    echo 'ok'
+                  '';
+                  serviceConfig = {
+                    User = user;
+                    WorkingDirectory = "${dataDir}";
+                    Group = user;
+                  };
+                };
+              };
+            };
+          };
       });
     };
 }
